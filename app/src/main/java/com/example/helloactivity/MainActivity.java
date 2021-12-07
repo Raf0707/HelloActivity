ccaeb7f5b53ce6287ec2e3f75f9d31f2a0f9e965
Raf <Raf_0707@mail.ru>
2021-12-07 21:41:53 +0300

    new Alpha animation

--- app/src/main/java/com/example/helloactivity/MainActivity.java
+++ app/src/main/java/com/example/helloactivity/MainActivity.java
@@ -10,6 +10,7 @@ import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.*;
 import android.widget.*;
 
 import com.example.helloactivity.R;
@@ -18,49 +19,38 @@ import com.example.helloactivity.HelloAnim;
 
 public class MainActivity extends AppCompatActivity {
 
-    HelloAnim secretTextView;
+    final int MENU_ALPHA_ID = 1;
+    TextView tv;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello);
 
-        secretTextView = findViewById(R.id.tvaa);
-        secretTextView.hide();
-        secretTextView.toggle();
-        secretTextView.setDuration(12000);
-        secretTextView.setIsVisible(false);
-        secretTextView.show();    // fade in
+        tv = findViewById(R.id.tva);
+        registerForContextMenu(tv);
+    }
 
-        secretTextView.hide();
-        secretTextView.setDuration(8000);
-        secretTextView.setText("اَلسَّلَامُ عَلَيْكُم وَرَحْمَةُ ٱللَّٰهِ تَعَالَى وَبَرَكَاتُه");
-        secretTextView.show();
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        switch (v.getId()) {
+            case R.id.tva:
+                menu.add(0, MENU_ALPHA_ID, 0, "alpha");
+                break;
+        }
+        super.onCreateContextMenu(menu, v, menuInfo);
+    }
 
-
-        /*
-        secretTextView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                secretTextView.show();    // fade in
-                secretTextView.hide();    // fade out
-                //secretTextView.toggle();  // fade in or out depending on current state
-
-                secretTextView.setText("اَلسَّلَامُ عَلَيْكُم وَرَحْمَةُ ٱللَّٰهِ تَعَالَى وَبَرَكَاتُه");
-            }
-
-        });
-
-         */
-/*
-        change = (Button) findViewById(R.id.change);
-        change.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                secretTextView.setText("اَلسَّلَامُ عَلَيْكُم وَرَحْمَةُ ٱللَّٰهِ تَعَالَى وَبَرَكَاتُه");
-            }
-        });
-*/
+    @Override
+    public boolean onContextItemSelected(MenuItem itemm) {
+        Animation anim = null;
+        switch (itemm.getItemId()) {
+            case MENU_ALPHA_ID:
+                anim = AnimationUtils.loadAnimation(this, R.anim.alpha);
+                break;
+        }
+        tv.startAnimation(anim);
+        return super.onContextItemSelected(itemm);
     }
 
 
